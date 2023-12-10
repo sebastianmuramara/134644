48858945c2bededee3dab3543979a3ec66ab9c53
sebastianmuramara <149579053+sebastianmuramara@users.noreply.github.com>
2023-12-10 06:30:10 +0300

    Add files via upload

---
+++ Chemistry Game Unity/Assets/Scripts/ExitLevel.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExitLevel : MonoBehaviour
+{
+
+    public void backToMenu()
+    {
+        SceneManager.LoadScene("Main_Menu_1");
+    }
+}
